94a6dfd1f6269a2a98479444160747810461a555
ellabook <ellabook@yilakanshu.local>
2019-08-31 11:23:47 +0800

    add build_json_msg()

--- rapidjson/json_test.cpp
+++ rapidjson/json_test.cpp
@@ -4,148 +4,228 @@
 #include "rapidjson/document.h"     // rapidjson's DOM-style API
 #include "rapidjson/prettywriter.h" // for stringify JSON
 #include <cstdio>
+#include <string>
 
 using namespace rapidjson;
 using namespace std;
 
-int main(int, char*[]) {
-    ////////////////////////////////////////////////////////////////////////////
-    // 1. Parse a JSON text string to a document.
+/*
+{
+    "name":"jack",//常规的
+    "age":18,
+    "sub":["a","b"],//value是数组
+    "elp":[ {"a":"A","b":"B"},//value是一个数组且里面每个元素又是一个json格式
+            {"c":"C","d":"D"},
+          ]
+}
+*/
 
-    const char json[] = " { \"hello\" : \"world\", \"t\" : true , \"f\" : false, \"n\": null, \"i\":123, \"pi\": 3.1416, \"a\":[1, 2, 3, 4] } ";
-    printf("Original JSON:\n %s\n", json);
+std::string build_json_msg()
+{
+  Document doc;
+  doc.SetObject();
+  Document::AllocatorType& a=doc.GetAllocator();
 
-    Document document;  // Default template parameter uses UTF8 and MemoryPoolAllocator.
+  doc.AddMember("name","jack",a);
+  doc.AddMember("age",18,a);
+  
+  Value sub(kArrayType);
+  sub.PushBack("a",a);
+  sub.PushBack("b",a);
+  doc.AddMember("sub",sub,a);
+
+  Value json(kArrayType);
+  Value obj(kObjectType);
+  obj.AddMember("a","A",a);
+  obj.AddMember("b","B",a);
+  json.PushBack(obj,a);
+
+  Value obj1(kObjectType);
+  obj1.AddMember("c","C",a);
+  obj1.AddMember("d","D",a);
+  json.PushBack(obj1,a);
+
+  doc.AddMember("elp",json,a);
+
+  StringBuffer s;
+  Writer<StringBuffer> writer(s);
+  doc.Accept(writer);
+  return std::string(s.GetString());
+}
+
+int main(int, char *[]) {
+
+#if 0
+  ////////////////////////////////////////////////////////////////////////////
+  // 1. Parse a JSON text string to a document.
+
+  const char json[] =
+      " { \"hello\" : \"world\", \"t\" : true , \"f\" : false, \"n\": null, "
+      "\"i\":123, \"pi\": 3.1416, \"a\":[1, 2, 3, 4] } ";
+  printf("Original JSON:\n %s\n", json);
+
+  Document
+      document; // Default template parameter uses UTF8 and MemoryPoolAllocator.
 
 #if 0
     // "normal" parsing, decode strings to new buffers. Can use other input stream via ParseStream().
     if (document.Parse(json).HasParseError())
         return 1;
 #else
-    // In-situ parsing, decode strings directly in the source string. Source must be string.
-    char buffer[sizeof(json)];
-    memcpy(buffer, json, sizeof(json));
-    if (document.ParseInsitu(buffer).HasParseError())
-        return 1;
+  // In-situ parsing, decode strings directly in the source string. Source must
+  // be string.
+  char buffer[sizeof(json)];
+  memcpy(buffer, json, sizeof(json));
+  if (document.ParseInsitu(buffer).HasParseError())
+    return 1;
 #endif
 
-    printf("\nParsing to document succeeded.\n");
+  printf("\nParsing to document succeeded.\n");
 
-    ////////////////////////////////////////////////////////////////////////////
-    // 2. Access values in document.
+  ////////////////////////////////////////////////////////////////////////////
+  // 2. Access values in document.
 
-    printf("\nAccess values in document:\n");
-    assert(document.IsObject());    // Document is a JSON value represents the root of DOM. Root can be either an object or array.
+  printf("\nAccess values in document:\n");
+  assert(document.IsObject()); // Document is a JSON value represents the root
+                               // of DOM. Root can be either an object or array.
 
-    assert(document.HasMember("hello"));
-    assert(document["hello"].IsString());
-    printf("hello = %s\n", document["hello"].GetString());
+  assert(document.HasMember("hello"));
+  assert(document["hello"].IsString());
+  printf("hello = %s\n", document["hello"].GetString());
 
-    // Since version 0.2, you can use single lookup to check the existing of member and its value:
-    Value::MemberIterator hello = document.FindMember("hello");
-    assert(hello != document.MemberEnd());
-    assert(hello->value.IsString());
-    assert(strcmp("world", hello->value.GetString()) == 0);
-    (void)hello;
+  // Since version 0.2, you can use single lookup to check the existing of
+  // member and its value:
+  Value::MemberIterator hello = document.FindMember("hello");
+  assert(hello != document.MemberEnd());
+  assert(hello->value.IsString());
+  assert(strcmp("world", hello->value.GetString()) == 0);
+  (void)hello;
 
-    assert(document["t"].IsBool());     // JSON true/false are bool. Can also uses more specific function IsTrue().
-    printf("t = %s\n", document["t"].GetBool() ? "true" : "false");
+  assert(document["t"].IsBool()); // JSON true/false are bool. Can also uses
+                                  // more specific function IsTrue().
+  printf("t = %s\n", document["t"].GetBool() ? "true" : "false");
 
-    assert(document["f"].IsBool());
-    printf("f = %s\n", document["f"].GetBool() ? "true" : "false");
+  assert(document["f"].IsBool());
+  printf("f = %s\n", document["f"].GetBool() ? "true" : "false");
 
-    printf("n = %s\n", document["n"].IsNull() ? "null" : "?");
+  printf("n = %s\n", document["n"].IsNull() ? "null" : "?");
 
-    assert(document["i"].IsNumber());   // Number is a JSON type, but C++ needs more specific type.
-    assert(document["i"].IsInt());      // In this case, IsUint()/IsInt64()/IsUint64() also return true.
-    printf("i = %d\n", document["i"].GetInt()); // Alternative (int)document["i"]
+  assert(document["i"].IsNumber()); // Number is a JSON type, but C++ needs more
+                                    // specific type.
+  assert(document["i"].IsInt()); // In this case, IsUint()/IsInt64()/IsUint64()
+                                 // also return true.
+  printf("i = %d\n", document["i"].GetInt()); // Alternative (int)document["i"]
 
-    assert(document["pi"].IsNumber());
-    assert(document["pi"].IsDouble());
-    printf("pi = %g\n", document["pi"].GetDouble());
+  assert(document["pi"].IsNumber());
+  assert(document["pi"].IsDouble());
+  printf("pi = %g\n", document["pi"].GetDouble());
 
+  {
+    const Value &a = document["a"]; // Using a reference for consecutive access
+                                    // is handy and faster.
+    assert(a.IsArray());
+    for (SizeType i = 0; i < a.Size();
+         i++) // rapidjson uses SizeType instead of size_t.
+      printf("a[%d] = %d\n", i, a[i].GetInt());
+
+    int y = a[0].GetInt();
+    (void)y;
+
+    // Iterating array with iterators
+    printf("a = ");
+    for (Value::ConstValueIterator itr = a.Begin(); itr != a.End(); ++itr)
+      printf("%d ", itr->GetInt());
+    printf("\n");
+
+    for(auto& v:a.GetArray())
     {
-        const Value& a = document["a"]; // Using a reference for consecutive access is handy and faster.
-        assert(a.IsArray());
-        for (SizeType i = 0; i < a.Size(); i++) // rapidjson uses SizeType instead of size_t.
-            printf("a[%d] = %d\n", i, a[i].GetInt());
-
-        int y = a[0].GetInt();
-        (void)y;
-
-        // Iterating array with iterators
-        printf("a = ");
-        for (Value::ConstValueIterator itr = a.Begin(); itr != a.End(); ++itr)
-            printf("%d ", itr->GetInt());
-        printf("\n");
+        v.GetInt();
     }
+  }
 
-    // Iterating object members
-    static const char* kTypeNames[] = { "Null", "False", "True", "Object", "Array", "String", "Number" };
-    for (Value::ConstMemberIterator itr = document.MemberBegin(); itr != document.MemberEnd(); ++itr)
-        printf("Type of member %s is %s\n", itr->name.GetString(), kTypeNames[itr->value.GetType()]);
+  // Iterating object members
+  static const char *kTypeNames[] = {"Null",  "False",  "True",  "Object",
+                                     "Array", "String", "Number"};
+  for (Value::ConstMemberIterator itr = document.MemberBegin();
+       itr != document.MemberEnd(); ++itr)
+    printf("Type of member %s is %s\n", itr->name.GetString(),
+           kTypeNames[itr->value.GetType()]);
 
-    ////////////////////////////////////////////////////////////////////////////
-    // 3. Modify values in document.
+  ////////////////////////////////////////////////////////////////////////////
+  // 3. Modify values in document.
 
-    // Change i to a bigger number
-    {
-        uint64_t f20 = 1;   // compute factorial of 20
-        for (uint64_t j = 1; j <= 20; j++)
-            f20 *= j;
-        document["i"] = f20;    // Alternate form: document["i"].SetUint64(f20)
-        assert(!document["i"].IsInt()); // No longer can be cast as int or uint.
-    }
+  // Change i to a bigger number
+  {
+    u_int64_t f20 = 1; // compute factorial of 20
+    for (u_int64_t j = 1; j <= 20; j++)
+      f20 *= j;
+    document["i"] = f20; // Alternate form: document["i"].SetUint64(f20)
+    assert(!document["i"].IsInt()); // No longer can be cast as int or uint.
+  }
 
-    // Adding values to array.
-    {
-        Value& a = document["a"];   // This time we uses non-const reference.
-        Document::AllocatorType& allocator = document.GetAllocator();
-        for (int i = 5; i <= 10; i++)
-            a.PushBack(i, allocator);   // May look a bit strange, allocator is needed for potentially realloc. We normally uses the document's.
+  // Adding values to array.
+  {
+    Value &a = document["a"]; // This time we uses non-const reference.
+    Document::AllocatorType &allocator = document.GetAllocator();
+    for (int i = 5; i <= 10; i++)
+      a.PushBack(
+          i,
+          allocator); // May look a bit strange, allocator is needed for
+                      // potentially realloc. We normally uses the document's.
 
-        // Fluent API
-        a.PushBack("Lua", allocator).PushBack("Mio", allocator);
-    }
+    // Fluent API
+    a.PushBack("Lua", allocator).PushBack("Mio", allocator);
+  }
 
-    // Making string values.
+  // Making string values.
 
-    // This version of SetString() just store the pointer to the string.
-    // So it is for literal and string that exists within value's life-cycle.
-    {
-        document["hello"] = "rapidjson";    // This will invoke strlen()
-        // Faster version:
-        // document["hello"].SetString("rapidjson", 9);
-    }
+  // This version of SetString() just store the pointer to the string.
+  // So it is for literal and string that exists within value's life-cycle.
+  {
+    document["hello"] = "rapidjson"; // This will invoke strlen()
+                                     // Faster version:
+    // document["hello"].SetString("rapidjson", 9);
+  }
 
-    // This version of SetString() needs an allocator, which means it will allocate a new buffer and copy the the string into the buffer.
-    Value author;
-    {
-        char buffer2[10];
-        int len = sprintf(buffer2, "%s %s", "Milo", "Yip");  // synthetic example of dynamically created string.
+  // This version of SetString() needs an allocator, which means it will
+  // allocate a new buffer and copy the the string into the buffer.
+  Value author;
+  {
+    char buffer2[10];
+    int len =
+        sprintf(buffer2, "%s %s", "Milo",
+                "Yip"); // synthetic example of dynamically created string.
 
-        author.SetString(buffer2, static_cast<SizeType>(len), document.GetAllocator());
-        // Shorter but slower version:
-        // document["hello"].SetString(buffer, document.GetAllocator());
+    author.SetString(buffer2, static_cast<SizeType>(len),
+                     document.GetAllocator());
+    // Shorter but slower version:
+    // document["hello"].SetString(buffer, document.GetAllocator());
 
-        // Constructor version:
-        // Value author(buffer, len, document.GetAllocator());
-        // Value author(buffer, document.GetAllocator());
-        memset(buffer2, 0, sizeof(buffer2)); // For demonstration purpose.
-    }
-    // Variable 'buffer' is unusable now but 'author' has already made a copy.
-    document.AddMember("author", author, document.GetAllocator());
+    // Constructor version:
+    // Value author(buffer, len, document.GetAllocator());
+    // Value author(buffer, document.GetAllocator());
+    memset(buffer2, 0, sizeof(buffer2)); // For demonstration purpose.
+  }
+  // Variable 'buffer' is unusable now but 'author' has already made a copy.
+  document.AddMember("author", author, document.GetAllocator());
 
-    assert(author.IsNull());        // Move semantic for assignment. After this variable is assigned as a member, the variable becomes null.
+  assert(
+      author.IsNull()); // Move semantic for assignment. After this variable is
+                        // assigned as a member, the variable becomes null.
 
-    ////////////////////////////////////////////////////////////////////////////
-    // 4. Stringify JSON
+  ////////////////////////////////////////////////////////////////////////////
+  // 4. Stringify JSON
 
-    printf("\nModified JSON with reformatting:\n");
-    StringBuffer sb;
-    PrettyWriter<StringBuffer> writer(sb);
-    document.Accept(writer);    // Accept() traverses the DOM and generates Handler events.
-    puts(sb.GetString());
+  printf("\nModified JSON with reformatting:\n");
+  StringBuffer sb;
+  PrettyWriter<StringBuffer> writer(sb);
+  document.Accept(
+      writer); // Accept() traverses the DOM and generates Handler events.
+  puts(sb.GetString());
+#else
+    std::string o=build_json_msg();
+    printf("dom is %s\n", o.c_str());
+#endif
 
-    return 0;
+  return 0;
 }
